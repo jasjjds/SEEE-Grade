4aaa0680fa306a085fae3d5861bc31cacf38eccb
jajjds <goagoagoa1st@gmail.com>
2025-12-14 18:19:55 +0700

    helo

---
+++ seee_grade/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: "SEEE Grade",
+  description: "Make by BIDH",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={roboto.className}>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
